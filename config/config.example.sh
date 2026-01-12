d9a55b619098abd333d401f5d2b89d438e6f7275
James <jamie.sambrook@gmail.com>
2026-01-12 21:38:05 +0000

    Small tweak to config

--- config/config.example.sh
+++ config/config.example.sh
@@ -2,7 +2,15 @@
 ICLOUD_VAULT_PATH="/Users/James/Library/Mobile Documents/com~apple~CloudDocs/Obsidian"
 
 # USB name
-USB_VOLUME_NAME="/Volumes/BACKUP_USB"
+USB_VOLUME_NAME="BACKUP_USB"
 
 # Where the vault lives on the USB
-USB_VAULT_SUBDIR="/Volumes/BACKUP_USB/Obsidian/"
+USB_VAULT_SUBDIR="Obsidian/"
+
+EXCLUDES=(
+  ".git/"
+  "logs/"
+  ".obsidian/cache/"
+  ".obsidian/workspace*"
+  ".trash/"
+)
