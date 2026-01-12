5a5826b2225f584ac275b40eb70a79d056954a14
James <jamie.sambrook@gmail.com>
2026-01-12 15:07:50 +0000

    Added configurations

---
+++ config/config.example.sh
@@ -0,0 +1,8 @@
+# Obsidian vault folder on iCloud
+ICLOUD_VAULT_PATH="/Users/James/Library/Mobile\ Documents/com\~apple\~CloudDocs/Obsidian"
+
+# USB name
+USB_VOLUME_NAME="/Volumes/BACKUP_USB"
+
+# Where the vault lives on the USB
+USB_VAULT_SUBDIR="/Volumes/BACKUP_USB/Obsidian/"
